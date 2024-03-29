77d65688aba25123c39d56ddc30696c6753e0ec2
MLUCAN2 <162901593+MLUCAN2@users.noreply.github.com>
2024-03-29 14:45:57 -0500

    updated javascript with code and notes to outline my process

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # update-payroll-manager
+## Description
+Updated a payroll tracker. Added a series of JavaScript code to pull and coallate data of entered employees and their salary. The code also allows for the display of the average salary and selection of a random employee in the console.
+
+## Installation
+N/A
+
+## Usage
+This site will allow the user to input employee names and salary.
+
+## Features
+Includes a display to enter employee names and salary, as well as a button to begin the operation. 
--- script.js
+++ script.js
@@ -1,19 +1,68 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
+//The console below will log the add employee button
+console.log(addEmployeesBtn)
+let addEmployee = true;
+//The empty array below will collect all of the employees once we "push". ML
+const employees = []
 
 // Collect employee data
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
+  //Using a while loop since there isn't a fixed number of iterations. ML
+  while (addEmployee) {
+    const firstName = prompt("Enter First Name:");
+    console.log("First Name", firstName);
+    const lastName = prompt("Enter Last Name:");
+    console.log("Last Name", lastName);
+    const salary = parseFloat(prompt("Enter Salary"));
+    console.log("Salary", salary);
+
+    const employee = {
+      firstName: firstName,
+      lastName: lastName,
+      salary: salary,
+    };
+
+    employees.push(employee);
+    const continueInput= prompt("Would you like to add an employee? (Yes/No)");
+    console.log("(Yes/No)", continueInput)
+    addEmployee = continueInput === 'yes';
+  }
+  return employees;
 }
 
+// TODO: Calculate and display the average salary
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
-  // TODO: Calculate and display the average salary
+  //Create a "let" to zero out the salary. ML
+  //Need a "for" loop that goes through each employee in the array and references back to the total salary. ML
+  //Once the loop goes through then I'll need to get the average and then reference back to the HTML. ML
+  //Set the display to text so that it shows the average. ML
+  //Console.log it to show the average. ML
+  let totalSalary = 0;
+  for (let index=0;index < employeesArray.length; index++){
+    totalSalary += employeesArray[index].salary;
+  }
+  const averageSalary = totalSalary/employeesArray.length;
+  const displayAverageSalary = document.querySelector('#employee-table');
+  displayAverageSalary.textContent = `Avarage Salary: ${averageSalary.toLocaleString}`;
+  console.log (`Avarage Salary: ${averageSalary.toLocaleString()}`)
 }
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
+  //Need to create an index between 0 and the original array to be able to make a selection. ML
+  //Create an random index to actually pull the employee. ML
+  //Reference back to the HTML element. ML
+  //Add in what you want to display (first/last name and salary).ML
+  //Need a console.log to show a random employee selected. ML
+const randomIndex= Math.floor(Math.random()*employeesArray.length);
+const randomEmployee=employeesArray[randomIndex];
+const randomEmployeeElement= document.querySelector('#employee-table');
+randomEmployeeElement.textContent= `Random Employee: ${randomEmployee.firstName}, ${randomEmployee.lastName}, ${randomEmployee.salary.toLocaleString()}`;
+console.log (`Random Employee: ${randomEmployee.firstName}, ${randomEmployee.lastName}, ${randomEmployee.salary.toLocaleString()}`);
 }
 
 /*
